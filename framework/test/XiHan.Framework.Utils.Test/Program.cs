ae8db124aa2316adc94d025b3db17a10ebe411c3
zhaifanhua <zhaifanhua@foxmail.com>
2024-11-18 22:30:41 +0530

    更新

--- framework/test/XiHan.Framework.Utils.Test/Program.cs
+++ framework/test/XiHan.Framework.Utils.Test/Program.cs
@@ -1,3 +1,11 @@
-﻿Console.WriteLine("Hello, Test!");
+﻿using XiHan.Framework.Utils.Security.Cryptography;
+
+Console.WriteLine("Hello, Test!");
+
+string ss = "{\"amount\":\"500\",\"merchantOrderNo\":\"20241118181906524010001\",\"customerName\":\"\",\"customerEmail\":\"\",\"customerPhone\":\"\",\"notifyUrl\":\"https://api.tkusdtapi.com/api/greenorder\"}";
+string aesKey = "NjD/j53SSqnnNd3v";
+string aesIv = "W7QzK1tQK9in04QY";
+string sign = AesHelper.Encrypt(ss, aesKey, aesIv);
+Console.WriteLine(sign);
 
 Console.ReadKey();
