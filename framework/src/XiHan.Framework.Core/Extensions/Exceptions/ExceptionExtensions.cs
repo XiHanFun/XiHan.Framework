e7b235a1d036a00f11af4112ce2bcc8a5944e81b
zhaifanhua <zhaifanhua@foxmail.com>
2024-11-27 05:05:49 +0800

    优化

--- framework/src/XiHan.Framework.Core/Extensions/Exceptions/ExceptionExtensions.cs
+++ framework/src/XiHan.Framework.Core/Extensions/Exceptions/ExceptionExtensions.cs
@@ -24,6 +24,42 @@ namespace XiHan.Framework.Core.Extensions.Exceptions;
 public static class ExceptionExtensions
 {
     /// <summary>
+    /// 获取异常的消息，包括内部异常
+    /// </summary>
+    /// <param name="exception">异常</param>
+    /// <param name="isHideStackTrace">是否隐藏异常规模信息</param>
+    /// <returns></returns>
+    public static string FormatMessage(this Exception exception, bool isHideStackTrace = false)
+    {
+        if (exception == null)
+        {
+            return string.Empty;
+        }
+        var message = exception.Message;
+        if (isHideStackTrace)
+        {
+            return message;
+        }
+        if (exception.InnerException != null)
+        {
+            message += " --> " + exception.InnerException.FormatMessage();
+        }
+        return message;
+    }
+
+    /// <summary>
+    /// 尝试从给定的<paramref name="exception"/>获取日志级别，如果它实现了<see cref="IHasLogLevel"/>接口
+    /// 否则，返回<paramref name="defaultLevel"/>
+    /// </summary>
+    /// <param name="exception">异常</param>
+    /// <param name="defaultLevel">默认日志级别</param>
+    /// <returns></returns>
+    public static LogLevel GetLogLevel(this Exception exception, LogLevel defaultLevel = LogLevel.Error)
+    {
+        return (exception as IHasLogLevel)?.LogLevel ?? defaultLevel;
+    }
+
+    /// <summary>
     /// 使用<see cref="ExceptionDispatchInfo.Capture"/>方法以重新抛出异常，同时保留堆栈跟踪
     /// </summary>
     /// <param name="exception">异常将被重新抛出</param>
@@ -33,14 +69,28 @@ public static class ExceptionExtensions
     }
 
     /// <summary>
-    /// 尝试从给定的<paramref name="exception"/>获取日志级别，如果它实现了<see cref="IHasLogLevel"/>接口
-    /// 否则，返回<paramref name="defaultLevel"/>
+    /// 如果<paramref name="isThrow"/>为true，则抛出异常
     /// </summary>
-    /// <param name="exception"></param>
-    /// <param name="defaultLevel"></param>
-    /// <returns></returns>
-    public static LogLevel GetLogLevel(this Exception exception, LogLevel defaultLevel = LogLevel.Error)
+    /// <param name="exception">异常</param>
+    /// <param name="isThrow">是否抛出异常</param>
+    public static void ThrowIf(this Exception exception, bool isThrow)
     {
-        return (exception as IHasLogLevel)?.LogLevel ?? defaultLevel;
+        if (isThrow)
+        {
+            throw exception;
+        }
+    }
+
+    /// <summary>
+    /// 如果<paramref name="isThrowFunc"/>返回true，则抛出异常
+    /// </summary>
+    /// <param name="exception">异常</param>
+    /// <param name="isThrowFunc">是否抛出异常</param>
+    public static void ThrowIf(this Exception exception, Func<bool> isThrowFunc)
+    {
+        if (isThrowFunc())
+        {
+            throw exception;
+        }
     }
 }
