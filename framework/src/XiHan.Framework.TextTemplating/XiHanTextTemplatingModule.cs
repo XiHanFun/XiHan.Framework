5dc2d2479d7552bb6c40834a39ec5cae9b3fa5e5
zhaifanhua <zhaifanhua@foxmail.com>
2024-12-16 03:53:41 +0800

    添加 XiHan.Framework.TextTemplating； 添加 XiHan.Framework.VirtualFileSystem；

---
+++ framework/src/XiHan.Framework.TextTemplating/XiHanTextTemplatingModule.cs
@@ -0,0 +1,35 @@
+﻿#region <<版权版本注释>>
+
+// ----------------------------------------------------------------
+// Copyright ©2024 ZhaiFanhua All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// FileName:XiHanTextTemplatingModule
+// Guid:957b2815-e1a0-4f9e-8023-1e5d68482316
+// Author:zhaifanhua
+// Email:me@zhaifanhua.com
+// CreateTime:2024/12/16 3:42:14
+// ----------------------------------------------------------------
+
+#endregion <<版权版本注释>>
+
+using XiHan.Framework.Core.Modularity;
+using XiHan.Framework.VirtualFileSystem;
+
+namespace XiHan.Framework.TextTemplating;
+
+/// <summary>
+/// XiHanTextTemplatingModule
+/// </summary>
+[DependsOn(
+    typeof(XiHanVirtualFileSystemModule)
+    )]
+public class XiHanTextTemplatingModule : XiHanModule
+{
+    /// <summary>
+    /// 服务配置
+    /// </summary>
+    /// <param name="context"></param>
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+    }
+}
---
+++ framework/src/XiHan.Framework.VirtualFileSystem/XiHanVirtualFileSystemModule.cs
@@ -0,0 +1,31 @@
+﻿#region <<版权版本注释>>
+
+// ----------------------------------------------------------------
+// Copyright ©2024 ZhaiFanhua All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// FileName:XiHanVirtualFileSystemModule
+// Guid:8543c204-0497-4b7a-be6b-6d06c7c3053b
+// Author:zhaifanhua
+// Email:me@zhaifanhua.com
+// CreateTime:2024/12/16 3:42:33
+// ----------------------------------------------------------------
+
+#endregion <<版权版本注释>>
+
+using XiHan.Framework.Core.Modularity;
+
+namespace XiHan.Framework.VirtualFileSystem;
+
+/// <summary>
+/// XiHanVirtualFileSystemModule
+/// </summary>
+public class XiHanVirtualFileSystemModule : XiHanModule
+{
+    /// <summary>
+    /// 服务配置
+    /// </summary>
+    /// <param name="context"></param>
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+    }
+}
