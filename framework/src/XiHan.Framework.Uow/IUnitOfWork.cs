390d60c93684b527e2639a837ea51c2caf45c716
afandawork <afandawork@gmail.com>
2025-01-06 20:56:03 +0530

    优化

--- framework/src/XiHan.Framework.Uow/IUnitOfWork.cs
+++ framework/src/XiHan.Framework.Uow/IUnitOfWork.cs
@@ -68,7 +68,7 @@ public interface IUnitOfWork : IDatabaseApiContainer, ITransactionApiContainer, 
     bool IsCompleted { get; }
 
     /// <summary>
-    /// 预
+    /// 预订名称
     /// </summary>
     string? ReservationName { get; }
 
