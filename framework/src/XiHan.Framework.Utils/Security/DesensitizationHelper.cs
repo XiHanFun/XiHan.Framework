31ea321325faea69f961cbd1ad1dd4efc4d714ef
afandawork <afandawork@gmail.com>
2025-02-22 20:24:33 +0530

    feat: 优化

--- framework/src/XiHan.Framework.Utils/Security/DesensitizationHelper.cs
+++ framework/src/XiHan.Framework.Utils/Security/DesensitizationHelper.cs
@@ -1,4 +1,18 @@
-﻿using System.Text.RegularExpressions;
+﻿#region <<版权版本注释>>
+
+// ----------------------------------------------------------------
+// Copyright ©2021-Present ZhaiFanhua All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// FileName:DesensitizationHelper
+// Guid:86738b17-6256-4f60-a9cd-016a7a471396
+// Author:afand
+// Email:me@zhaifanhua.com
+// CreateTime:2025/02/22 14:28:00
+// ----------------------------------------------------------------
+
+#endregion <<版权版本注释>>
+
+using System.Text.RegularExpressions;
 using XiHan.Framework.Utils.Verifications;
 
 namespace XiHan.Framework.Utils.Security;
