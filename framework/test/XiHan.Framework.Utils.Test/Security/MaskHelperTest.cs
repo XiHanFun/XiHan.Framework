18d0b67965772137b08de59bbd20bbe0c9d6096b
afandawork <afandawork@gmail.com>
2025-04-25 13:31:00 +0530

    优化

---
+++ framework/test/XiHan.Framework.Utils.Test/Security/MaskHelperTest.cs
@@ -0,0 +1,22 @@
+﻿#region <<版权版本注释>>
+
+// ----------------------------------------------------------------
+// Copyright ©2021-Present ZhaiFanhua All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// FileName:MaskHelperTest
+// Guid:8727b533-dc2e-4d20-b6d9-fd39a43c8ad1
+// Author:zhaifanhua
+// Email:me@zhaifanhua.com
+// CreateTime:2025/4/24 16:08:48
+// ----------------------------------------------------------------
+
+#endregion <<版权版本注释>>
+
+namespace XiHan.Framework.Utils.Test.Security;
+
+/// <summary>
+/// MaskHelperTest
+/// </summary>
+public class MaskHelperTest
+{
+}
