a9628100c61d3dd21d1a098588464ab428f6eb20
afandawork <afandawork@gmail.com>
2025-04-27 15:36:18 +0530

    style: 调整目录结构

--- framework/src/XiHan.Framework.Utils/Net/Sse/SseServerOptions.cs
+++ framework/src/XiHan.Framework.Utils/Net/Sse/SseServerOptions.cs
@@ -15,62 +15,6 @@
 namespace XiHan.Framework.Utils.Net.Sse;
 
 /// <summary>
-/// SSE服务器处理上下文
-/// </summary>
-public class SseEventContext
-{
-    /// <summary>
-    /// 初始化SSE事件上下文
-    /// </summary>
-    /// <param name="stream">输出流</param>
-    /// <param name="lastEventId">最后事件ID</param>
-    /// <param name="server">服务器实例</param>
-    public SseEventContext(Stream stream, string? lastEventId, SseServer server)
-    {
-        Stream = stream;
-        LastEventId = lastEventId;
-        Server = server;
-    }
-
-    /// <summary>
-    /// 输出流
-    /// </summary>
-    public Stream Stream { get; }
-
-    /// <summary>
-    /// 最后事件ID
-    /// </summary>
-    public string? LastEventId { get; }
-
-    /// <summary>
-    /// SSE服务器实例
-    /// </summary>
-    public SseServer Server { get; }
-
-    /// <summary>
-    /// 发送事件
-    /// </summary>
-    /// <param name="data">数据内容</param>
-    /// <param name="eventType">事件类型</param>
-    /// <param name="id">事件ID</param>
-    /// <returns>异步任务</returns>
-    public Task SendEventAsync(string data, string? eventType = null, string? id = null)
-    {
-        return SseServer.SendEventAsync(Stream, data, eventType, id);
-    }
-
-    /// <summary>
-    /// 发送事件
-    /// </summary>
-    /// <param name="message">SSE消息</param>
-    /// <returns>异步任务</returns>
-    public Task SendEventAsync(SseMessage message)
-    {
-        return SseServer.SendEventAsync(Stream, message);
-    }
-}
-
-/// <summary>
 /// SSE服务器配置选项
 /// </summary>
 public class SseServerOptions
---
+++ framework/src/XiHan.Framework.Utils/Net/Ssh/SshCommandResult.cs
@@ -0,0 +1,46 @@
+﻿#region <<版权版本注释>>
+
+// ----------------------------------------------------------------
+// Copyright ©2021-Present ZhaiFanhua All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// FileName:SshCommandResult
+// Guid:f2f6f25a-6cf0-401b-9769-51f7e9eb8dda
+// Author:zhaifanhua
+// Email:me@zhaifanhua.com
+// CreateTime:2025/4/27 15:32:34
+// ----------------------------------------------------------------
+
+#endregion <<版权版本注释>>
+
+namespace XiHan.Framework.Utils.Net.Ssh;
+
+/// <summary>
+/// SSH命令执行结果
+/// </summary>
+public class SshCommandResult
+{
+    /// <summary>
+    /// 执行的命令
+    /// </summary>
+    public string Command { get; set; } = "";
+
+    /// <summary>
+    /// 执行结果
+    /// </summary>
+    public string Result { get; set; } = "";
+
+    /// <summary>
+    /// 错误信息
+    /// </summary>
+    public string Error { get; set; } = "";
+
+    /// <summary>
+    /// 退出状态码
+    /// </summary>
+    public int ExitCode { get; set; }
+
+    /// <summary>
+    /// 是否执行成功
+    /// </summary>
+    public bool IsSuccess => ExitCode == 0;
+}
