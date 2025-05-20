8d487f2244e4b6f10d7d624084cb1a4b2a5e5945
afandawork <afandawork@gmail.com>
2025-05-20 13:03:25 +0530

    feat: 添加 XiHan.Framework.Validation、XiHan.Framework.Authorization 包

---
+++ framework/src/XiHan.Framework.Validation/XiHanValidationModule.cs
@@ -0,0 +1,31 @@
+﻿#region <<版权版本注释>>
+
+// ----------------------------------------------------------------
+// Copyright ©2021-Present ZhaiFanhua All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// FileName:XiHanValidationModule
+// Guid:f08d98d7-2ff2-484b-aa45-acf3d88c0c09
+// Author:zhaifanhua
+// Email:me@zhaifanhua.com
+// CreateTime:2025/05/06 4:53:17
+// ----------------------------------------------------------------
+
+#endregion <<版权版本注释>>
+
+using XiHan.Framework.Core.Modularity;
+
+namespace XiHan.Framework.Data;
+
+/// <summary>
+/// 曦寒框架数据校验模块
+/// </summary>
+public class XiHanValidationModule : XiHanModule
+{
+    /// <summary>
+    /// 服务配置
+    /// </summary>
+    /// <param name="context"></param>
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+    }
+}
