9e7fb0a95122f639fcc0fa6498f0b6f771e694b0
afandawork <afandawork@gmail.com>
2025-05-23 21:52:13 +0530

    feat: 优化编码方案，添加 Base32、Base36、Base32、Base58、Base62、Base95、CustomRadix 编码

---
+++ framework/src/XiHan.Framework.Utils/System/Converters/Base62.cs
@@ -0,0 +1,109 @@
+﻿#region <<版权版本注释>>
+
+// ----------------------------------------------------------------
+// Copyright ©2021-Present ZhaiFanhua All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// FileName:Base62
+// Guid:200dd36c-2d20-40f3-be57-fa44648984f5
+// Author:zhaifanhua
+// Email:me@zhaifanhua.com
+// CreateTime:2025/5/23 20:43:10
+// ----------------------------------------------------------------
+
+#endregion <<版权版本注释>>
+
+using System.Numerics;
+using System.Text;
+
+namespace XiHan.Framework.Utils.System.Converters;
+
+/// <summary>
+/// Base62 编码解码
+/// </summary>
+/// <remarks>
+/// 主要特点：包含数字+大小写字母，较紧凑，编码长度更短
+/// 常见用途：短链接、邀请码、用户ID编码等
+/// </remarks>
+public static class Base62
+{
+    // 字符表：62 个字符（0-9A-Za-z）
+    private const string Base62Alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    private static readonly Dictionary<char, int> _charMap = [];
+
+    static Base62()
+    {
+        for (var i = 0; i < Base62Alphabet.Length; i++)
+        {
+            _charMap[Base62Alphabet[i]] = i;
+        }
+    }
+
+    /// <summary>
+    /// 编码 byte 数组为 Base62 字符串
+    /// </summary>
+    public static string Encode(byte[] input)
+    {
+        var intData = new BigInteger(input.Concat(new byte[] { 0 }).ToArray()); // 防止负数
+        var result = new StringBuilder();
+        while (intData > 0)
+        {
+            var remainder = (int)(intData % 62);
+            intData /= 62;
+            result.Insert(0, Base62Alphabet[remainder]);
+        }
+        return result.ToString();
+    }
+
+    /// <summary>
+    /// 解码 Base62 字符串为 byte 数组
+    /// </summary>
+    public static byte[] Decode(string input)
+    {
+        BigInteger intData = 0;
+        foreach (var c in input)
+        {
+            intData = (intData * 62) + _charMap[c];
+        }
+
+        var bytes = intData.ToByteArray();
+        if (bytes[^1] == 0)
+        {
+            bytes = [.. bytes.Take(bytes.Length - 1)]; // 移除补零
+        }
+
+        return bytes;
+    }
+
+    /// <summary>
+    /// 编码 long 值为 Base62 字符串
+    /// </summary>
+    public static string EncodeLong(long input)
+    {
+        if (input == 0)
+        {
+            return "0";
+        }
+
+        var result = new StringBuilder();
+        while (input > 0)
+        {
+            result.Insert(0, Base62Alphabet[(int)(input % 62)]);
+            input /= 62;
+        }
+        return result.ToString();
+    }
+
+    /// <summary>
+    /// 解码 Base62 字符串为 long 值
+    /// </summary>
+    public static long DecodeLong(string input)
+    {
+        long result = 0;
+        foreach (var c in input)
+        {
+            result = (result * 62) + _charMap[c];
+        }
+        return result;
+    }
+}
--- framework/src/XiHan.Framework.Utils/Text/EncodingExtensions.cs
+++ framework/src/XiHan.Framework.Utils/Text/EncodingExtensions.cs
@@ -16,6 +16,7 @@ using System.Globalization;
 using System.Net;
 using System.Text;
 using System.Web;
+using XiHan.Framework.Utils.System.Converters;
 using XiHan.Framework.Utils.Verifications;
 
 namespace XiHan.Framework.Utils.Text;
@@ -33,7 +34,7 @@ public static class EncodingExtensions
     public static string Base32Encode(this string data)
     {
         var bytes = Encoding.UTF8.GetBytes(data);
-        return Base32.ToBase32String(bytes);
+        return Base32.Encode(bytes);
     }
 
     /// <summary>
@@ -43,7 +44,7 @@ public static class EncodingExtensions
     /// <returns>解码后的字符串</returns>
     public static string Base32Decode(this string data)
     {
-        var bytes = Base32.FromBase32String(data);
+        var bytes = Base32.Decode(data);
         return Encoding.UTF8.GetString(bytes);
     }
 
@@ -209,110 +210,5 @@ public static class EncodingExtensions
         {
             return string.Empty;
         }
-    }
-}
-
-/// <summary>
-/// 手写 Base32 转换
-/// </summary>
-public static class Base32
-{
-    private const string Base32Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
-
-    /// <summary>
-    /// 二进制转换为 Base32
-    /// </summary>
-    /// <param name="bytes"></param>
-    /// <returns></returns>
-    /// <exception cref="ArgumentNullException"></exception>
-    public static string ToBase32String(byte[] bytes)
-    {
-        ArgumentNullException.ThrowIfNull(bytes);
-
-        if (bytes.Length == 0)
-        {
-            return string.Empty;
-        }
-
-        StringBuilder sb = new(((bytes.Length * 8) + 4) / 5);
-
-        var bitCount = 0;
-        var accumulatedBits = 0;
-
-        foreach (var currentByte in bytes)
-        {
-            accumulatedBits |= currentByte << bitCount;
-            bitCount += 8;
-            while (bitCount >= 5)
-            {
-                const int Mask = 0x1f;
-                var currentBase32Value = accumulatedBits & Mask;
-                _ = sb.Append(Base32Alphabet[currentBase32Value]);
-                accumulatedBits >>= 5;
-                bitCount -= 5;
-            }
-        }
-
-        if (bitCount <= 0)
-        {
-            return sb.ToString();
-        }
-
-        {
-            const int Mask = 0x1f;
-            var currentBase32Value = accumulatedBits & Mask;
-            _ = sb.Append(Base32Alphabet[currentBase32Value]);
-        }
-
-        return sb.ToString();
-    }
-
-    /// <summary>
-    /// Base32 转换为二进制
-    /// </summary>
-    /// <param name="base32String"></param>
-    /// <returns></returns>
-    /// <exception cref="ArgumentNullException"></exception>
-    /// <exception cref="ArgumentException"></exception>
-    public static byte[] FromBase32String(string base32String)
-    {
-        ArgumentNullException.ThrowIfNull(base32String);
-
-        if (base32String.Length == 0)
-        {
-            return [];
-        }
-
-        base32String = base32String.TrimEnd('=');
-
-        var byteCount = base32String.Length * 5 / 8;
-        var buffer = new byte[byteCount];
-
-        var bitCount = 0;
-        var accumulatedBits = 0;
-        var bufferIndex = 0;
-        foreach (var currentCharValue in base32String.Select(currentChar => Base32Alphabet.IndexOf(currentChar)))
-        {
-            if (currentCharValue is < 0 or > 31)
-            {
-                throw new ArgumentException("Invalid character in Base32 string.");
-            }
-
-            accumulatedBits |= currentCharValue << bitCount;
-            bitCount += 5;
-
-            if (bitCount < 8)
-            {
-                continue;
-            }
-
-            const int Mask = 0xff;
-            var currentByteValue = accumulatedBits & Mask;
-            buffer[bufferIndex++] = (byte)currentByteValue;
-            accumulatedBits >>= 8;
-            bitCount -= 8;
-        }
-
-        return buffer;
     }
 }
