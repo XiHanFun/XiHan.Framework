e28d4b874d01c5e4c397f8a9187bbeeffdca5237
zhaifanhua <zhaifanhua@foxmail.com>
2025-06-03 03:24:27 +0800

    feat: 添加 XiHan.Framework.Data 包

---
+++ framework/src/XiHan.Framework.Data/XiHanDataModule.cs
@@ -0,0 +1,31 @@
+﻿#region <<版权版本注释>>
+
+// ----------------------------------------------------------------
+// Copyright ©2021-Present ZhaiFanhua All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// FileName:XiHanDataModule
+// Guid:a991cdf9-5a89-45db-b781-c9dfebb4d538
+// Author:zhaifanhua
+// Email:me@zhaifanhua.com
+// CreateTime:2025/6/3 2:46:52
+// ----------------------------------------------------------------
+
+#endregion <<版权版本注释>>
+
+using XiHan.Framework.Core.Modularity;
+
+namespace XiHan.Framework.Data;
+
+/// <summary>
+/// 曦寒数据访问模块
+/// </summary>
+public class XiHanDataModule : XiHanModule
+{
+    /// <summary>
+    /// 服务配置
+    /// </summary>
+    /// <param name="context"></param>
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+    }
+}
