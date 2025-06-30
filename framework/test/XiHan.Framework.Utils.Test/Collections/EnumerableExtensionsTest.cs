84c31d75488c27547feca4084770315b0740cfb7
zhaifanhua <zhaifanhua@foxmail.com>
2025-07-01 05:56:43 +0800

    feat: 移除测试代码

--- framework/test/XiHan.Framework.Utils.Test/Collections/EnumerableExtensionsTest.cs
+++
@@ -1,9 +0,0 @@
-﻿using JetBrains.Annotations;
-using XiHan.Framework.Utils.Collections;
-
-namespace XiHan.Framework.Utils.Test.Collections;
-
-[TestSubject(typeof(EnumerableExtensions))]
-public class EnumerableExtensionsTest
-{
-}
