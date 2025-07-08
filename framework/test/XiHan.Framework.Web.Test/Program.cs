9799ad827f751e8d72d2f6d251a7679fdb96ea08
zhaifanhua <zhaifanhua@foxmail.com>
2025-07-09 07:15:15 +0800

    feat: 移除测试

--- framework/test/XiHan.Framework.Web.Test/Program.cs
+++ framework/test/XiHan.Framework.Web.Test/Program.cs
@@ -1,7 +1,6 @@
 ﻿using Serilog;
 using XiHan.Framework.AspNetCore.Extensions.DependencyInjection;
 using XiHan.Framework.Core.Extensions.DependencyInjection;
-using XiHan.Framework.Http.Extensions;
 using XiHan.Framework.Web.Test;
 
 try
@@ -13,18 +12,6 @@ try
     var app = builder.Build();
 
     await app.InitializeApplicationAsync();
-
-    var totalSize = 100000000;
-    var progress = new Progress<long>(downloaded =>
-    {
-        var percent = totalSize > 0 ? downloaded * 100 / totalSize : 0;
-        Console.WriteLine($"下载进度：{percent}% ({downloaded}/{totalSize} bytes)");
-    });
-
-    var fileName = "normal2ssbump.exe";
-    var savePath = Path.Combine("C:\\Users\\zhaifanhua\\Downloads", fileName);
-    var url = "https://onmun.s3.ap-east-1.amazonaws.com/normal2ssbump.exe";
-    var response = await url.DownloadAsync(savePath, progress);
 
     await app.RunAsync();
 
