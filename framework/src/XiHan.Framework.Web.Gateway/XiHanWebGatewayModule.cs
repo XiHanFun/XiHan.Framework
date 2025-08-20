e9b05ededf29f03c906b920b0fb671c107377ed1
zhaifanhua <zhaifanhua@foxmail.com>
2025-08-21 05:31:24 +0800

    feat: 优化

--- framework/src/XiHan.Framework.Web.Gateway/XiHanWebGatewayModule.cs
+++ framework/src/XiHan.Framework.Web.Gateway/XiHanWebGatewayModule.cs
@@ -3,7 +3,7 @@
 // ----------------------------------------------------------------
 // Copyright ©2021-Present ZhaiFanhua All Rights Reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
-// FileName:XiHanGatewayModule
+// FileName:XiHanWebGatewayModule
 // Guid:13d9d929-d3de-4447-972e-715992171319
 // Author:zhaifanhua
 // Email:me@zhaifanhua.com
@@ -14,12 +14,12 @@
 
 using XiHan.Framework.Core.Modularity;
 
-namespace XiHan.Framework.Gateway;
+namespace XiHan.Framework.Web.Gateway;
 
 /// <summary>
-/// 曦寒框架网关模块
+/// 曦寒框架 Web 网关模块
 /// </summary>
-public class XiHanGatewayModule : XiHanModule
+public class XiHanWebGatewayModule : XiHanModule
 {
     /// <summary>
     /// 服务配置
