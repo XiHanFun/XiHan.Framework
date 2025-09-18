40489337e41bce41cc0835d9e115218dbc660018
zhaifanhua <zhaifanhua@foxmail.com>
2025-09-19 04:10:07 +0800

    feat: 计划任务重命名

--- framework/src/XiHan.Framework.Tasks/ScheduledJobs/XiHanScheduledJobBase.cs
+++ framework/src/XiHan.Framework.Tasks/ScheduledJobs/XiHanScheduledJobBase.cs
@@ -3,7 +3,7 @@
 // ----------------------------------------------------------------
 // Copyright ©2021-Present ZhaiFanhua All Rights Reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
-// FileName:XiHanJobBase
+// FileName:XiHanScheduledJobBase
 // Guid:849e8610-0a52-4da8-aba4-ac1e71724502
 // Author:zhaifanhua
 // Email:me@zhaifanhua.com
@@ -12,11 +12,11 @@
 
 #endregion <<版权版本注释>>
 
-namespace XiHan.Framework.Tasks.Jobs;
+namespace XiHan.Framework.Tasks.ScheduledJobs;
 
 /// <summary>
-/// XiHanJobBase
+/// XiHanScheduledJobBase
 /// </summary>
-public class XiHanJobBase
+public class XiHanScheduledJobBase
 {
 }
