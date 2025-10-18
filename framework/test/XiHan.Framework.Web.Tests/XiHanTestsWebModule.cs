5fe9b59041d22f7d7280f19bb54cc4ba0a624a0a
zhaifanhua <zhaifanhua@foxmail.com>
2025-10-19 06:11:43 +0800

    test: 全量包测试依赖情况

--- framework/test/XiHan.Framework.Web.Tests/XiHanTestsWebModule.cs
+++ framework/test/XiHan.Framework.Web.Tests/XiHanTestsWebModule.cs
@@ -12,12 +12,41 @@
 
 #endregion <<版权版本注释>>
 
+using XiHan.Framework.AI;
+using XiHan.Framework.Authentication;
+using XiHan.Framework.Authorization;
+using XiHan.Framework.Tasks;
+using XiHan.Framework.Bot;
+using XiHan.Framework.Caching;
+using XiHan.Framework.CodeGeneration;
 using XiHan.Framework.Core.Application;
 using XiHan.Framework.Core.Modularity;
+using XiHan.Framework.Data;
+using XiHan.Framework.Domain;
+using XiHan.Framework.DistributedIds;
+using XiHan.Framework.EventBus;
+using XiHan.Framework.Http;
+using XiHan.Framework.Localization;
 using XiHan.Framework.Logging;
+using XiHan.Framework.Messaging;
+using XiHan.Framework.MultiTenancy;
+using XiHan.Framework.ObjectMapping;
+using XiHan.Framework.Script;
+using XiHan.Framework.SearchEngines;
+using XiHan.Framework.Security;
+using XiHan.Framework.Serialization;
+using XiHan.Framework.Settings;
+using XiHan.Framework.Templating;
+using XiHan.Framework.Threading;
+using XiHan.Framework.Uow;
+using XiHan.Framework.Validation;
+using XiHan.Framework.VirtualFileSystem;
+using XiHan.Framework.Application;
 using XiHan.Framework.Web.Api;
 using XiHan.Framework.Web.Core;
 using XiHan.Framework.Web.Docs;
+using XiHan.Framework.Web.Gateway;
+using XiHan.Framework.Web.RealTime;
 
 namespace XiHan.Framework.Web.Tests;
 
@@ -25,10 +54,39 @@ namespace XiHan.Framework.Web.Tests;
 /// 曦寒测试应用 Web 主机
 /// </summary>
 [DependsOn(
+    typeof(XiHanAIModule),
+    typeof(XiHanAuthenticationModule),
+    typeof(XiHanAuthorizationModule),
+    typeof(XiHanTasksModule),
+    typeof(XiHanBotModule),
+    typeof(XiHanCachingModule),
+    typeof(XiHanCodeGenerationModule),
+    typeof(XiHanDataModule),
+    typeof(XiHanDomainModule),
+    typeof(XiHanDistributedIdsModule),
+    typeof(XiHanEventBusModule),
+    typeof(XiHanHttpModule),
+    typeof(XiHanLocalizationModule),
     typeof(XiHanLoggingModule),
+    typeof(XiHanMessagingModule),
+    typeof(XiHanMultiTenancyModule),
+    typeof(XiHanObjectMappingModule),
+    typeof(XiHanScriptModule),
+    typeof(XiHanSearchEnginesModule),
+    typeof(XiHanSecurityModule),
+    typeof(XiHanSerializationModule),
+    typeof(XiHanSettingsModule),
+    typeof(XiHanTemplatingModule),
+    typeof(XiHanThreadingModule),
+    typeof(XiHanUowModule),
+    typeof(XiHanValidationModule),
+    typeof(XiHanVirtualFileSystemModule),
+    typeof(XiHanApplicationModule),
     typeof(XiHanWebCoreModule),
     typeof(XiHanWebApiModule),
-    typeof(XiHanWebDocsModule)
+    typeof(XiHanWebDocsModule),
+    typeof(XiHanWebRealTimeModule),
+    typeof(XiHanWebGatewayModule)
 )]
 public class XiHanTestsWebModule : XiHanModule
 {
