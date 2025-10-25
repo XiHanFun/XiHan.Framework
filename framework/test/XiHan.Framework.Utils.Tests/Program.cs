721c6f35d1b3fb2f364bb1612f1a30b57d07cbf7
afandawork <afandawork@gmail.com>
2025-10-25 23:47:03 +0800

    test: 优化

--- framework/test/XiHan.Framework.Utils.Tests/Program.cs
+++ framework/test/XiHan.Framework.Utils.Tests/Program.cs
@@ -1,5 +1,6 @@
 ﻿using XiHan.Framework.Utils.Tests.ConsoleTools;
+using XiHan.Framework.Utils.Tests.Serialization.Yaml;
 
-Examples.ProgressBarExample().GetAwaiter().GetResult();
+//Examples.ProgressBarExample().GetAwaiter().GetResult();
 
 Console.ReadKey();
---
+++ framework/test/XiHan.Framework.Utils.Tests/Serialization/Yaml/YamlTest.cs
@@ -0,0 +1,63 @@
+﻿#region <<版权版本注释>>
+
+// ----------------------------------------------------------------
+// Copyright ©2021-Present ZhaiFanhua All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// FileName:YamlTest
+// Guid:beb9cbd3-7cdd-4134-b7e3-f7f2dd252b0c
+// Author:zhaifanhua
+// Email:me@zhaifanhua.com
+// CreateTime:2025/10/25 20:34:38
+// ----------------------------------------------------------------
+
+#endregion <<版权版本注释>>
+
+using XiHan.Framework.Utils.Serialization.Yaml;
+
+namespace XiHan.Framework.Utils.Tests.Serialization.Yaml;
+
+/// <summary>
+/// YamlTest
+/// </summary>
+public class YamlTest
+{
+    public string JsonString = """
+        {
+            "Name": "ZhaiFanhua",
+            "Age": 18,
+            "Hobby": [
+                "Coding",
+                "Reading",
+                "Gaming"
+            ],
+            "IsStudent": false,
+            "Address": {
+                "City": "Shanghai",
+                "Country": "China"
+            }
+        }
+        """;
+
+    public string YamlString = """
+        Name: ZhaiFanhua
+        Age: 18
+        Hobby:
+          - Coding
+          - Reading
+          - Gaming
+        IsStudent: false
+        Address:
+          City: Shanghai
+          Country: China
+        """;
+
+    public string ToYamlString()
+    {
+        return JsonString.ToYaml();
+    }
+
+    public string ToJsonString()
+    {
+        return YamlString.ToJson();
+    }
+}
