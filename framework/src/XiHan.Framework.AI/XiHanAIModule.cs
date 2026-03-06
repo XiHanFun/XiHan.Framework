583cdaf930d1e63925b52e628a756d61f77c8100
zhaifanhua <zhaifanhua@foxmail.com>
2026-03-07 05:57:32 +0800

    feat: 重写 ai 功能

--- framework/src/XiHan.Framework.AI/XiHanAIModule.cs
+++ framework/src/XiHan.Framework.AI/XiHanAIModule.cs
@@ -12,20 +12,11 @@
 
 #endregion <<版权版本注释>>
 
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.SemanticKernel;
-using XiHan.Framework.AI.Extensions.DependencyInjection;
-using XiHan.Framework.AI.Options;
-using XiHan.Framework.AI.Providers;
-using XiHan.Framework.AI.Providers.Ollama;
-using XiHan.Framework.AI.Providers.OpenAI;
 using XiHan.Framework.Core.Extensions.DependencyInjection;
 using XiHan.Framework.Core.Modularity;
 using XiHan.Framework.Http;
 
 namespace XiHan.Framework.AI;
-
-#pragma warning disable SKEXP0010
 
 /// <summary>
 /// 曦寒框架 AI 扩展模块
@@ -43,40 +34,5 @@ public class XiHanAIModule : XiHanModule
     {
         var services = context.Services;
         var config = services.GetConfiguration();
-
-        services.AddXiHanAI(config);
-
-        // 注册 Semantic Kernel 内核，配置 Ollama、OpenAI 等 Connector
-        var kernelBuilder = services.AddKernel();
-
-        // Ollama
-        PostConfigure<OllamaOptions>(ollamaOptions =>
-        {
-            kernelBuilder.AddOllamaChatCompletion(
-                modelId: ollamaOptions.ModelName,
-                endpoint: new Uri(ollamaOptions.BaseUrl),
-                serviceId: ollamaOptions.ServiceId);
-            kernelBuilder.AddOllamaEmbeddingGenerator(
-                modelId: ollamaOptions.ModelName,
-                endpoint: new Uri(ollamaOptions.BaseUrl),
-                serviceId: ollamaOptions.ServiceId);
-            services.AddKeyedTransient<IXiHanAIService, XiHanOllamaService>(ollamaOptions.ServiceId);
-        });
-        // OpenAI
-        PostConfigure<OpenAIOptions>(openAiOptions =>
-        {
-            kernelBuilder.AddOpenAIChatCompletion(
-                modelId: openAiOptions.ModelName,
-                endpoint: new Uri(openAiOptions.BaseUrl),
-                apiKey: openAiOptions.ApiKey,
-                serviceId: openAiOptions.ServiceId);
-            kernelBuilder.AddOpenAIEmbeddingGenerator(
-                modelId: openAiOptions.ModelName,
-                apiKey: openAiOptions.ApiKey,
-                serviceId: openAiOptions.ServiceId);
-            services.AddKeyedTransient<IXiHanAIService, XiHanOpenAIService>(openAiOptions.ServiceId);
-        });
     }
 }
-
-#pragma warning restore SKEXP0010
