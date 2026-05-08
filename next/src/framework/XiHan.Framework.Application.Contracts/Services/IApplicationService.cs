16813f0c2ff4e9f437c55615e70f200c16eb5b78
zhaifanhua <zhaifanhua@foxmail.com>
2026-05-09 02:36:56 +0800

    feat: next重建

--- next/src/framework/XiHan.Framework.Application.Contracts/Services/IApplicationService.cs
+++
@@ -1,8 +0,0 @@
-namespace XiHan.Framework.Application.Contracts.Services;
-
-/// <summary>
-/// 表示应用服务标记接口。
-/// </summary>
-public interface IApplicationService
-{
-}
--- next/src/framework/XiHan.Framework.Web/XiHanFrameworkWebMarker.cs
+++
@@ -1,8 +0,0 @@
-namespace XiHan.Framework.Web;
-
-/// <summary>
-/// 提供 Web 程序集标记类型，便于架构测试定位程序集。
-/// </summary>
-public sealed class XiHanFrameworkWebMarker
-{
-}
